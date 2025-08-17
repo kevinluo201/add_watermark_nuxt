c8848ef697625ad4e7b427d77ca7cfbcf50d6b72
kevinluo201 <kevin.luo@hey.com>
2025-08-17 07:11:01 -0400

    show gtm only in production

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,18 +16,20 @@ export default defineNuxtConfig({
           src: 'https://kit.fontawesome.com/4335c2f54b.js',
           crossorigin: 'anonymous'
         },
-        {
-          src: 'https://www.googletagmanager.com/gtag/js?id=G-XLRM21CEWV',
-          async: true
-        },
-        {
-          innerHTML: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag() { dataLayer.push(arguments); }
-            gtag('js', new Date());
-            gtag('config', 'G-XLRM21CEWV');
-          `
-        }
+        ...(process.env.NODE_ENV === 'production' ? [
+          {
+            src: 'https://www.googletagmanager.com/gtag/js?id=G-XLRM21CEWV',
+            async: true
+          },
+          {
+            innerHTML: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag() { dataLayer.push(arguments); }
+              gtag('js', new Date());
+              gtag('config', 'G-XLRM21CEWV');
+            `
+          }
+        ] : [])
       ]
     }
   },
